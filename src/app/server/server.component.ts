d8ddf1fcbdebbf4bfc10098bee50d9043fd9a63d
theertha <theertha@bititude.com>
2022-01-18 22:05:33 +0530

    directives added

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css'],
+  styleUrls: ['./server.component.css']
 })
+
 export class ServerComponent implements OnInit {
-  serverName = ''
-  allowNewServer = false
-  propertyTitle = 'this is propertyTitle'
-  serverCreationStatus = 'no server was created'
-  serverCreated = false
+  serverName = '';
+  allowNewServer = false;
+  propertyTitle = 'this is propertyTitle';
+  serverCreationStatus = 'no server was created';
+  serverCreated=false;
 
   sayHello() {
     console.log('welcome to event binding')
@@ -21,7 +22,7 @@ export class ServerComponent implements OnInit {
   inputBox() {
     console.log('this is input box')
   }
-  username = ''
+  username="";
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true
@@ -29,10 +30,12 @@ export class ServerComponent implements OnInit {
   }
 
   ngOnInit() {}
-  onCreateServer(event: any) {
-    alert(event.target.innerHTML)
-    this.serverCreationStatus =
-      'server was created! server name is ' + this.serverName
+  // onCreateServer(event: any) {
+  //   alert(event.target.innerHTML)
+  //   this.serverCreationStatus="server was created! server name is "+this.serverName
+  // }
+  onCreateServer(){
+    this.serverCreated=true;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value
